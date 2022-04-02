d01e1e8f04e4572258fd3f5afe38652f8615c1a4
longtc9c2000 <65326062+longtc9c2000@users.noreply.github.com>
2022-04-02 23:52:18 +0700

    up

---
+++ components/CurrentWheather.js
@@ -0,0 +1,10 @@
+import {Fragment} from 'react'
+
+export default function CurrentWheather(props){
+    <Fragment>
+        <div>
+            sdasd
+        </div>
+        <p>{props.current}</p>
+    </Fragment>
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+require("dotenv").config();
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["openweathermap.org"],
+  },
+};
