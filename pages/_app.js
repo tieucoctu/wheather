869cc1d6aa51c33a384d79cfb9096c83479a9bff
longtc9c2000 <65326062+longtc9c2000@users.noreply.github.com>
2022-04-09 22:50:35 +0700

    su dung router chuyen huong trang nhanh hon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.scss'
+import "../styles/globals.scss";
+import { useEffect } from "react";
+import nprogress from "nprogress";
+import Router from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const start = () => nprogress.start;
+    const end = () => nprogress.done;
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+
+    return () => {
+      Router.events.on("routeChangeStart", start);
+      Router.events.on("routeChangeComplete", end);
+      Router.events.on("routeChangeError", end);
+    };
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
